7b6062465b354a49cae3bc80b8db74655931cd58
kiraamaa <kira.a.maclean@gmail.com>
2017-01-20 09:43:40 -0500

    Add fingerprint for ember leaflet to render markers

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,15 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    fingerprint: {
+    exclude: [
+      'images/layers-2x.png',
+      'images/layers.png',
+      'images/marker-icon-2x.png',
+      'images/marker-icon.png',
+      'images/marker-shadow.png'
+    ]
+  },
     sassOptions: {
       includePaths: [
         'bower_components/bootstrap-sass/assets/stylesheets'
